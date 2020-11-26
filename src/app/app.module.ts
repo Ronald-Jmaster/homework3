d90b09e3aa4098544c28116ec93b88f5e0f21091
Roman_Pasternak <Roman_Pasternak@epam.com>
2020-11-26 22:30:23 +0200

    homework 4-5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {FrontPageModule} from "./pages/front-page/front-page.module";
-import {HomeModule} from "./pages/home/home.module";
-import {AdminModule} from "./pages/admin/admin.module";
-
 @NgModule({
   declarations: [
     AppComponent
